709aa4e35578c5c9e3389712e21ecaa84c008461
Diego Alloza González <sralloza@gmail.com>
2021-10-01 22:53:42 +0200

    feat: add tenacity retry control (#2)
    * chore: add tenacity to reqs
    
    * feat: add tenacity retry control

--- reboot_notifier/notify.py
+++ reboot_notifier/notify.py
@@ -1,8 +1,23 @@
+import sys
+
+from tenacity import retry
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_fixed
+
 from .config import settings
 from .networking import session
 
 
+def retry_callback(retry_state):
+    print("RETRY ERROR:", retry_state, file=sys.stderr)
+    sys.exit(1)
+
+
+@retry(
+    stop=stop_after_attempt(10), wait=wait_fixed(10), retry_error_callback=retry_callback
+)
 def notify_text(msg: str):
+    raise ValueError
     for chat_id in settings.chat_ids:
         response = session.post(
             f"https://api.telegram.org/bot{settings.bot_token}/sendMessage",
